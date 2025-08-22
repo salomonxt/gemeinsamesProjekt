3a29b0bf3e7ffc59d36bc1858e52b363e685e3f8
salomonxt <salomonxt@protonmail.ch>
2025-08-22 16:48:45 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi,wer hat ein kleines Beispiel für Arrays?");
+        System.out.println("Hi,wer hat ein (kleines) Beispiel für Arrays?");
     }
 }
