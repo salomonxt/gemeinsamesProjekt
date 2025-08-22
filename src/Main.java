e444b72519a3d3e7d1644d414c75adad67e9ef76
Yaroslava <yaroslava.moskalova@gmail.com>
2025-08-22 17:27:52 +0200

    added array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi,wer hat ein (kleines) Beispiel für Arrays?");
+
+        Integer[] numbers = new Integer[4884];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
     }
 }
