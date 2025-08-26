bd68a243c5fd2fadea33365b314c58bf394bd459
salomonxt <salomonxt@protonmail.ch>
2025-08-26 17:53:29 +0200

    add struktur geordnet - platz für erweiterungen unten frei

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //26.8.17h09
+        //17:49
         System.out.println();
         System.out.println("Wer hat ein (kleines) Beispiel für Arrays?");
-        System.out.println("Einfach hier anhängen :D");      
-        System.out.println("====");
-        System.out.println("Salo_BSP1:");
-        System.out.println("=====");
+        System.out.println("-einfach unten anhängen-");
 
+        System.out.println("====");
+        System.out.println("S_BSP1:");
+        System.out.println("====");
+
+        int[] nullreihe = new int[3];
+
+        for (int i=0; i<=10; i++) {
+            System.out.println(java.util.Arrays.toString(nullreihe));
+            //hmm...
+        }
+
+        System.out.println("====");
+        System.out.println("Y_BSP1:");
+        System.out.println("====");
 
         Integer[] numbers = new Integer[4884];
 
@@ -19,11 +31,20 @@ public class Main {
 
         System.out.println(Arrays.toString(numbers));
 
- 
-        int[] sterne = new int[10];
-        for (int i=0; i<=10; i++) { 
-            System.out.println(java.util.Arrays.toString(sterne));
-            }
+
+
+
+
+        /*System.out.println("====");
+        System.out.println("Dein_BSP1:");
+        System.out.println("====");
+
+        CODE
+
+        System.out.println("====");
+        System.out.println("Dein_BSP2:");
+        System.out.println("====");
+*/
 
 
     }
