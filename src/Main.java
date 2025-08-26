3dff63848fe6fe459637df1fe3d42a7bb2221d3e
salomon <salomonxt@protonmail.ch>
2025-08-26 17:11:38 +0200

    Update Main.java
    etwas sortierter mit der Titel oben
    
    (wir könnten noch jeweilge codebereiche mit //Y   //Y
    bzw //S    //S markieren)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi,wer hat ein (kleines) Beispiel für Arrays?");
+        //26.8.17h09
+        System.out.println();
+        System.out.println("Wer hat ein (kleines) Beispiel für Arrays?");
+        System.out.println("Einfach hier anhängen :D");      
+        System.out.println("====");
+        System.out.println("Salo_BSP1:");
+        System.out.println("=====");
 
 
         Integer[] numbers = new Integer[4884];
@@ -13,13 +19,6 @@ public class Main {
 
         System.out.println(Arrays.toString(numbers));
 
-        System.out.println("Einfach hier rein :D");
-
-        System.out.println();
-        System.out.println("====");
-        System.out.println("Salo_BSP1:");
-        System.out.println("=====");
-
  
         int[] sterne = new int[10];
         for (int i=0; i<=10; i++) { 
@@ -29,4 +28,4 @@ public class Main {
 
     }
 }
-//hmm
+//
